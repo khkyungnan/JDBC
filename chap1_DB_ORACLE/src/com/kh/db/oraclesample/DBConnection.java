55546ed759860170288c8d62c65e85c40435bf00
khkyungnan <143380461+khkyungnan@users.noreply.github.com>
2023-10-17 15:53:20 +0900

    ff

--- chap1_DB_ORACLE/src/com/kh/db/oraclesample/DBConnection.java
+++ chap1_DB_ORACLE/src/com/kh/db/oraclesample/DBConnection.java
@@ -10,6 +10,10 @@ import java.sql.SQLException;
 public class DBConnection {
 
 	public static void main(String[] args) {
+		selectIf();
+	}
+	
+	static void selectBank() {
 		
 		//1. 드라이버 연결 : Oracle JDBC 드라이버 클래스 이름
 		String driver = "oracle.jdbc.driver.OracleDriver";
@@ -49,15 +53,67 @@ public class DBConnection {
 			e.printStackTrace();
 		}
 	}
+	static void selectKhcafe() {
+		//1. 드라이버 연결 : Oracle JDBC 드라이버 클래스 이름
+				String driver = "oracle.jdbc.driver.OracleDriver";
+		//2. 오라클 내 컴퓨터 연결 : 데이터 베이스 연결 정보
+		//                              나의IP주소:port번호
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String user = "khcafe";
+		String password = "kh1234";
+		Connection con = null;
+		//select * from menu 작성해서 menu table 가져오기
+	}
 
+	static void selectIf() {
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String user = "khbank";
+		String password = "kh1234";
+		Connection con = null;
+		
+		try {
+			con = DriverManager.getConnection(url, user, password);
+			//where절 사용하여 조건추가
+			String selectQuery = "SELECT * FROM BANK WHERE account_number in (?,?)";
+			PreparedStatement selectState = con.prepareStatement(selectQuery);
+			/*
+			 /여기에 원하는 조건의 account_id 설정
+			int targetAID = 2;
+
+			//조건 설정
+			selectState.setInt(1, targetAID);
+			 * */
+			String[] targerAN = {"1234567890","5555666777"};
+			selectState.setString(1, targerAN[0]);
+			selectState.setString(2, targerAN[1]);
+			ResultSet result = selectState.executeQuery();
+			
+			//값 존재 여부
+			if (!result.isBeforeFirst()) {
+				System.out.println("존재하는 데이터가 없습니다.");
+			}
+			while (result.next()) {
+				  int a = result.getInt("account_id");
+		    	    String b = result.getString("account_number");
+		    	    String c = result.getString("account_name");
+		    	    double d = result.getDouble("balance");
+		    	    String e = result.getString("branch_name");
+		    	    Date f = result.getDate("last_transaction_date");
+		    	    
+		    	    System.out.println("ACCOUNT_ID : " + a + ", ACCOUNT_NAME : " + b + ", BALANCE : " + c
+		    	            + " ACCOUNT_NUMBER " + d + " BRANCH_NAME " + e + " LastTransactionDate : " + f);
+			
+			} 
+			 if (!result.isBeforeFirst()) {
+	                System.out.println("데이터 없음");
+	            }
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 }
-
-
-
-
-
-
-
 
 
 
