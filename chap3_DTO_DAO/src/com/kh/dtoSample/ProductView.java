830cee27191b0b8b679934349095c12334fa02df
khkyungnan <143380461+khkyungnan@users.noreply.github.com>
2023-10-23 09:52:32 +0900

    FF

---
+++ chap3_DTO_DAO/src/com/kh/dtoSample/ProductView.java
@@ -0,0 +1,30 @@
+package com.kh.dtoSample;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Scanner;
+
+public class ProductView {
+	//출력 메소드 void
+	
+	public void displayProducts(List<ProductDTO> products) {
+		//향상된 for문
+		for(ProductDTO  p : products) {
+			System.out.println("Products ID : " + p.getProduct_id());
+			System.out.println("Products Name : " + p.getProductName());
+			System.out.println("Category : " + p.getCategory());
+			System.out.println("Price : " + p.getPrice());
+			System.out.println("Stock_quantity : " + p.getStock_quantity());
+		}
+		
+	}
+	
+}
+
+
+
+
+
+
+
+
